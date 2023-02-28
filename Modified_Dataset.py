a723fbb739d6c54fe48d341c41daf3c94448f207
Miguel Sargento <miguel.costa01@hotmail.com>
2023-02-28 13:28:28 +0000

    New Functions and Changes

---
+++ Modified_Dataset.py
@@ -0,0 +1,43 @@
+import mysql.connector
+import pandas as pd
+from Functions.AlertP1.data_cleaning import *
+from Functions.NLP.alertp1_nlp import *
+
+#Login Credentials
+"""
+File must follow this structure:
+"username"
+"password"
+"host"
+"database"
+port
+"""
+
+df = pd.read_csv("credentials.txt", sep=" ", header=None, names=["Value"])
+
+#Connection to the database
+host = df["Value"][2]
+user = df["Value"][0]
+password = df["Value"][1]
+database = df["Value"][3]
+port = df["Value"][4]
+mydb = mysql.connector.connect(host=host, user=user, database=database, port=port, password=password, auth_plugin='mysql_native_password')
+mycursor = mydb.cursor()
+
+#Safecheck to guarantee that the connection worked
+mycursor.execute('SHOW TABLES;')
+print(f"Tables: {mycursor.fetchall()}")
+print(mydb.connection_id) #it'll give connection_id,if got connected
+
+#Create a DataFrame
+alertP1 = pd.read_sql("""SELECT * FROM consultaneurologia201216anon_true""",mydb)
+
+#Data Cleaning
+date_format_alertP1(alertP1)
+replace_blank(alertP1)
+
+#NLP
+remove_stop_words(alertP1, "Texto", "clean_text")
+spacy_lemmatizer2(alertP1, "clean_text", "clean_text")
+
+print(alertP1["clean_text"].head())
