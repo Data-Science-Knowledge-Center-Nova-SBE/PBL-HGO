0309370ce66a4c1d777015a710aca1ce4de78362
Miguel Sargento <miguel.costa01@hotmail.com>
2023-03-17 17:37:57 +0000

    changes

--- Functions/connection/connection.py
+++ Functions/connection/connection.py
@@ -11,17 +11,22 @@ def connection(creds_path):
    "database"
    port
    """
-   creds = pd.read_csv(creds_path, sep=" ", header=None, names=["Value"])
-   host = creds['value'][0]
-   user = creds['value'][1]
-   password = creds['value'][2]
-   database = creds['value'][3]
-   port = creds['value'][4]
+   df = pd.read_csv(creds_path, sep=" ", header=None, names=["Value"])
+
+   #Connection to the database
+   host = df["Value"][2]
+   user = df["Value"][0]
+   password = df["Value"][1]
+   database = df["Value"][3]
+   port = df["Value"][4]
    mydb = mysql.connector.connect(host=host, user=user, database=database, port=port, password=password, auth_plugin='mysql_native_password')
    mycursor = mydb.cursor()
+
    #Safecheck to guarantee that the connection worked
    mycursor.execute('SHOW TABLES;')
    print(f"Tables: {mycursor.fetchall()}")
    print(mydb.connection_id) #it'll give connection_id,if got connected
+   
    alertP1 = pd.read_sql("""SELECT * FROM consultaneurologia201216anon_true""",mydb)
    return(alertP1)
+
