8cde4f825c9a2bdb98c52aede588643e8c350f76
marwenkm1 <53833@novasbe.pt>
2023-04-19 02:33:27 +0100

    updates

--- Functions/Models/Random_forest.py
+++ Functions/Models/Random_forest.py
@@ -1,5 +1,5 @@
 # import the classes
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
@@ -10,14 +10,13 @@ import seaborn as sns
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
-#logistic regression 
-def log_regression(features,target):
+#random forest
+def random_forest(features,target):
     # split X and y into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2,random_state=16,shuffle=False)
     # instantiate the model (using the default parameters)
-    logreg = LogisticRegression(random_state=16)
+    logreg =RandomForestClassifier(n_estimators=500, random_state=42)
     # fit the model with data
     clf=logreg.fit(X_train, y_train)
     y_pred = logreg.predict(X_test)
-    return y_pred,clf.coef_, clf.intercept_,X_train, X_test, y_train, y_test
-
+    return y_pred, clf.feature_importances_,X_train, X_test, y_train, y_test
