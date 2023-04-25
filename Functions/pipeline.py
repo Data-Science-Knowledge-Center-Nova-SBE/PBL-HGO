d11af5b28edc4e518b294a226e037be688aefe80
Utku Serdaroglu <serdarogluutku@gmail.com>
2023-04-25 18:41:45 +0100

    uıo

--- Functions/pipeline.py
+++ Functions/pipeline.py
@@ -35,31 +35,31 @@ def pre_process(df):
 
     pipeline = Pipeline([
         #Data Cleaning
-        #('Date Format', FunctionTransformer(date_format_alertP1)),
-        #('Replace Blanks', FunctionTransformer(replace_blank)),
-        #('Duplicated Entities', FunctionTransformer(entity_duplicated)),
-        #('Lower Case Text ', FunctionTransformer(lowering_text)),
-        #('Target Variable', FunctionTransformer(result)),
-        #('Sort Values', FunctionTransformer(sort_values)),
+        ('Date Format', FunctionTransformer(date_format_alertP1)),
+        ('Replace Blanks', FunctionTransformer(replace_blank)),
+        ('Duplicated Entities', FunctionTransformer(entity_duplicated)),
+        ('Lower Case Text ', FunctionTransformer(lowering_text)),
+        ('Target Variable', FunctionTransformer(result)),
+        ('Sort Values', FunctionTransformer(sort_values)),
         #Structured Features
-        #('Accepted Before', FunctionTransformer(bef_accepted)),
-        #('Area Classification', FunctionTransformer(class_area)),
-        #('Text Length', FunctionTransformer(text_length)),
-        #('Referral Steps', FunctionTransformer(referral_steps)),
-        #('Speciality', FunctionTransformer(speciality)),
-        #('Unit', FunctionTransformer(unit)),
+        ('Accepted Before', FunctionTransformer(bef_accepted)),
+        ('Area Classification', FunctionTransformer(class_area)),
+        ('Text Length', FunctionTransformer(text_length)),
+        ('Referral Steps', FunctionTransformer(referral_steps)),
+        ('Speciality', FunctionTransformer(speciality)),
+        ('Unit', FunctionTransformer(unit)),
         #NLP Features
-        #('symptoms_column', FunctionTransformer(symptoms_column)),
-        #('exams', FunctionTransformer(exams)),
-        #('comorbidities', FunctionTransformer(comorbidities)),
-        #('medication_column', FunctionTransformer(medication_column)),
-        #('Medication Total Count', FunctionTransformer(medication_count)),
-        #('Medication Concentration', FunctionTransformer(medication_concentration)),
-        #('synonyms', FunctionTransformer(synonyms)),
-        #('Retrieve original text', FunctionTransformer(lowering_text)),
-        #('clean_text', FunctionTransformer(clean_text)),
+        ('symptoms_column', FunctionTransformer(symptoms_column)),
+        ('exams', FunctionTransformer(exams)),
+        ('comorbidities', FunctionTransformer(comorbidities)),
+        ('medication_column', FunctionTransformer(medication_column)),
+        ('Medication Total Count', FunctionTransformer(medication_count)),
+        ('Medication Concentration', FunctionTransformer(medication_concentration)),
+        ('synonyms', FunctionTransformer(synonyms)),
+        ('Retrieve original text', FunctionTransformer(lowering_text)),
+        ('clean_text', FunctionTransformer(clean_text)),
         #Dummies
-        #('Dummies', FunctionTransformer(structured_data_dummies)),
+        ('Dummies', FunctionTransformer(structured_data_dummies)),
         #Keep only text rows
         ('Text Only', FunctionTransformer(text_only))
     ])
