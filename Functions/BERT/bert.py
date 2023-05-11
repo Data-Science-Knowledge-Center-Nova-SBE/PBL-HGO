f34dc81cf87e767a83fb72311d8710b20155ae1f
gabrabib <eugenioabib@gmail.com>
2023-05-11 23:46:14 +0100

    bert

--- Functions/BERT/bert.py
+++ Functions/BERT/bert.py
@@ -5,25 +5,26 @@ from sklearn.metrics.pairwise import cosine_similarity
 from transformers import AutoTokenizer, AutoModel   # for BERT
 
 
-def bert_embedding(dataset, column, acceptance_list, prefix, model_name='sentence-transformers/multi-qa-MiniLM-L6-cos-v1'):
+def bert_embedding(dataset, column, baseline_list, prefix, model_name='sentence-transformers/multi-qa-MiniLM-L6-cos-v1'):
 
-
+    # Get the sentences from the dataset (to be compared to the basiline list)
     referrals = dataset[column].tolist()
 
-    for index, protocol in enumerate(acceptance_list):
-
+    # Initialize the BERT tokenizer per sentence on the baseline list
+    for index, protocol in enumerate(baseline_list):
+        # initialize tokenizer and model
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = AutoModel.from_pretrained(model_name)
 
+        # create list of sentences to be encoded, being the first the baseline protocol
         sentences = [protocol] + referrals
-
-        # the following line is only necessary if you want to limit the number of sentences. MUST BE COMMENTED OUT TO RUN ON FULL DATASET
-        # sentences = sentences[:100]
 
         # initialize dictionary to store tokenized sentences
         tokens = {'input_ids': [], 'attention_mask': []}
 
+        # loop through sentences
         for sentence in sentences:
+
             # encode each sentence and append to dictionary
             new_tokens = tokenizer.encode_plus(sentence, max_length=128,
                                             truncation=True, padding='max_length',
@@ -34,37 +35,43 @@ def bert_embedding(dataset, column, acceptance_list, prefix, model_name='sentenc
         # reformat list of tensors into single tensor
         tokens['input_ids'] = torch.stack(tokens['input_ids'])
         tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
-            
+
+        # run through model and get embeddings    
         outputs = model(**tokens)
         outputs.keys()
+
         print(index)
+        # get embeddings from last hidden state
         embeddings = outputs.last_hidden_state
-        # embeddings
 
+        # get attention mask  
         attention_mask = tokens['attention_mask']
-        # attention_mask.shape
+
+        # create mask for embeddings
         mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
-        # mask.shape
 
+        # mask embeddings
         masked_embeddings = embeddings * mask
-        # masked_embeddings.shape
 
+        # sum the embeddings along the axis of the tokens
         summed = torch.sum(masked_embeddings, 1)
-        # summed.shape
 
+        # sum the number of tokens that should be considered in each sentence
         summed_mask = torch.clamp(mask.sum(1), min=1e-9)
         summed_mask.shape
 
+        # divide the summed embeddings by the number of tokens that should be considered in each sentence
         mean_pooled = summed / summed_mask
 
         # convert from PyTorch tensor to numpy array
         mean_pooled = mean_pooled.detach().numpy()
 
-        # calculate
+        # calculate cosine similarity between baseline protocol and referrals
         results = cosine_similarity(
             [mean_pooled[0]],
             mean_pooled[1:]
         )
+        # add results to dataset
         column_name = prefix + str(index)
         dataset[column_name] = results[0]
 
